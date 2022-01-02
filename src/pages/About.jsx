d8d0a19b6c853821afc53c3f9f198da9a55da0f0
우동진 <wjinh93@gmail.com>
2022-01-02 18:45:34 +0900

    layout pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p>A React app to search GitHub profiles and see profile details.</p>
+    </div>
+  )
+}
+
+export default About
